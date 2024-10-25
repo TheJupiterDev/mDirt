a74838d59236228dc1ae63a540a7e2731c75cbd7
Jupiter Dev <141870656+TheJupiterDev@users.noreply.github.com>
2024-10-25 16:15:29 -0500

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
+import json.tool
 import os
 import sys
 import shutil
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPixmap
 from tkinter import filedialog
 from ui import Ui_MainWindow
+import json
 
 class app():
     def __init__(self):
         super(app, self).__init__()
         
         self.blocks = {}
+        self.texture = {"0": "", "1": "", "2": "", "3": "", "4": "", "5": ""}
 
         self.app = QtWidgets.QApplication(sys.argv)
         self.app.setStyle("Fusion")
@@ -18,12 +22,59 @@ class app():
         self.ui.setupUi(self.mainwindow)
         self.mainwindow.show()
 
+        self.ui.actionImport_from_mrdt.triggered.connect(self.imported)
+        self.ui.actionExport_to_mrdt_2.triggered.connect(self.export)
+
         self.checkAdd()
         self.checkRemove()
         self.checkGenerate()
         self.checkResourceEnabled()
+        self.checkTextures()
 
         self.app.exec()
+    
+    def checkTextures(self):
+        self.ui.topFaceBtn.clicked.connect(lambda: self.getTexture("4"))
+        self.ui.bottomFaceBtn.clicked.connect(lambda: self.getTexture("5"))
+        self.ui.rightFaceBtn.clicked.connect(lambda: self.getTexture("1"))
+        self.ui.leftFaceBtn.clicked.connect(lambda: self.getTexture("3"))
+        self.ui.frontFaceBtn.clicked.connect(lambda: self.getTexture("2"))
+        self.ui.backFaceBtn.clicked.connect(lambda: self.getTexture("0"))
+    
+    def getTexture(self, id):
+        self.id = id
+        self.texture[self.id] = filedialog.askopenfile(filetypes=[("PNG File", "*.png")], defaultextension=[("PNG File", "*.png")]).name
+        self.pixmap = QPixmap(self.texture[self.id]).scaled(41, 41, aspectRatioMode=True)
+        if self.id == "0":
+            self.ui.backFace.setPixmap(self.pixmap)
+        elif self.id == "1":
+            self.ui.rightFace.setPixmap(self.pixmap)
+        elif self.id == "2":
+            self.ui.frontFace.setPixmap(self.pixmap)
+        elif self.id == "3":
+            self.ui.leftFace.setPixmap(self.pixmap)
+        elif self.id == "4":
+            self.ui.topFace.setPixmap(self.pixmap)
+        elif self.id == "5":
+            self.ui.bottomFace.setPixmap(self.pixmap)
+        
+    
+    def imported(self):
+        self.iFile = filedialog.askopenfile(filetypes=[("mDirt File", "*.mdrt")], defaultextension=[("mDirt File", "*.mdrt")])  #.replace("/","\\")
+        self.packProperties = json.load(self.iFile)
+        self.ui.packName.setText(self.packProperties["packName"])
+        self.ui.packNamespace.setText(self.packProperties["packNamespace"])
+        self.ui.packDescription.setText(self.packProperties["packDescription"])
+        self.ui.packVersion.setCurrentText(self.packProperties["packVersion"])
+        self.blocks = self.packProperties["blcks"]
+        for self.block in self.blocks:
+            self.ui.blockList.addItem(self.blocks[self.block]["name"])
+        
+    def export(self):
+        self.eFile = filedialog.asksaveasfile(filetypes=[("mDirt File", "*.mdrt")], defaultextension=[("mDirt File", "*.mdrt")])
+        self.packProperties = {"packName": self.ui.packName.text(), "packNamespace": self.ui.packNamespace.text(), "packDescription": self.ui.packDescription.text(), "packVersion": self.ui.packVersion.currentText(), "blcks": self.blocks}
+        self.eFile.write(str(self.packProperties).replace("'", '"'))
+        self.eFile.close()
     
     def checkAdd(self):
         self.ui.buttonAddBlock.clicked.connect(self.addBlock)
@@ -39,34 +90,60 @@ class app():
         if self.ui.blockResourceCheckBox.isChecked():
             self.ui.blockTextureLabel.setEnabled(True)
             self.ui.blockModelLabel.setEnabled(True)
-            self.ui.blockTexturePath.setEnabled(True)
             self.ui.blockModel.setEnabled(True)
         else:
             self.ui.blockTextureLabel.setEnabled(False)
             self.ui.blockModelLabel.setEnabled(False)
-            self.ui.blockTexturePath.setEnabled(False)
             self.ui.blockModel.setEnabled(False)
     
     def toggleResource(self):
         if self.ui.blockResourceCheckBox.isChecked():
             self.ui.blockTextureLabel.setEnabled(True)
             self.ui.blockModelLabel.setEnabled(True)
-            self.ui.blockTexturePath.setEnabled(True)
             self.ui.blockModel.setEnabled(True)
+            self.ui.backFace.setEnabled(True)
+            self.ui.backFaceBtn.setEnabled(True)
+            self.ui.frontFace.setEnabled(True)
+            self.ui.frontFaceBtn.setEnabled(True)
+            self.ui.leftFace.setEnabled(True)
+            self.ui.leftFaceBtn.setEnabled(True)
+            self.ui.rightFace.setEnabled(True)
+            self.ui.rightFaceBtn.setEnabled(True)
+            self.ui.topFace.setEnabled(True)
+            self.ui.topFaceBtn.setEnabled(True)
+            self.ui.bottomFace.setEnabled(True)
+            self.ui.bottomFaceBtn.setEnabled(True)
         else:
             self.ui.blockTextureLabel.setEnabled(False)
             self.ui.blockModelLabel.setEnabled(False)
-            self.ui.blockTexturePath.setEnabled(False)
             self.ui.blockModel.setEnabled(False)
+            self.ui.backFace.setEnabled(False)
+            self.ui.backFaceBtn.setEnabled(False)
+            self.ui.frontFace.setEnabled(False)
+            self.ui.frontFaceBtn.setEnabled(False)
+            self.ui.leftFace.setEnabled(False)
+            self.ui.leftFaceBtn.setEnabled(False)
+            self.ui.rightFace.setEnabled(False)
+            self.ui.rightFaceBtn.setEnabled(False)
+            self.ui.topFace.setEnabled(False)
+            self.ui.topFaceBtn.setEnabled(False)
+            self.ui.bottomFace.setEnabled(False)
+            self.ui.bottomFaceBtn.setEnabled(False)
 
     def addBlock(self):
+        self.textureNames = {"0": "", "1": "", "2": "", "3": "", "4": "", "5": ""}
+        for text in self.texture.keys():
+            self.val = self.texture[text]
+            self.textureNames[text] = os.path.splitext(os.path.basename(str(self.val)))[0]
+        
         self.blockProperties = {
             "name": self.ui.blockName.text(),
             "displayName": self.ui.blockDisplayName.text(),
             "customModelData": self.ui.blockCMD.text(),
             "baseBlock": self.ui.blockBase.text(),
-            "texturePath": self.ui.blockTexturePath.text(),
-            "blockDrop": self.ui.blockDrop.text()
+            "blockDrop": self.ui.blockDrop.text(),
+            "texturePaths": self.texture,
+            "textures": self.textureNames
         }
 
         self.blocks[self.ui.blockName.text()] = self.blockProperties
@@ -100,11 +177,13 @@ class app():
                     file.write(',')
             file.write(']}')
             file.close()
-        for block in self.blocks:
-            self.texture = self.packDir + "\\assets\\minecraft\\textures\\item\\" + self.blocks[block]["name"] + ".png"
-            shutil.copy(self.blocks[block]["texturePath"], self.texture)
+        for self.block in self.blocks:
+            self.texture = self.packDir + "\\assets\\minecraft\\textures\\item\\" #+ self.blocks[block]["name"] + ".png"
+            for path in self.blocks[block]["texturePaths"].values():
+                if not os.path.exists(os.path.join(self.texture, os.path.splitext(os.path.basename(str(path)))[-2] + ".png")):
+                    shutil.copy(path, os.path.join(self.texture, os.path.splitext(os.path.basename(str(path)))[-2] + ".png"))
             with open(f'{self.packDir}\\assets\\minecraft\\models\\'+ self.nameSpace + '\\' + self.blocks[block]["name"] + '.json', 'w') as file:
-                file.write('{"credit": "Made with mDirt","textures": {"0": "item/' + self.blocks[block]["name"] + '","particle": "item/' + self.blocks[block]["name"] + '"},"elements": [{"from": [0, 0, 0],"to": [16, 16, 16],"faces": {"north": {"uv": [0, 0, 16, 16], "texture": "#0"},"east": {"uv": [0, 0, 16, 16], "texture": "#0"},"south": {"uv": [0, 0, 16, 16], "texture": "#0"},"west": {"uv": [0, 0, 16, 16], "texture": "#0"},"up": {"uv": [0, 0, 16, 16], "texture": "#0"},"down": {"uv": [0, 0, 16, 16], "texture": "#0"}}}],"display": {"thirdperson_righthand": {"rotation": [0, 0, -55],"translation": [0, 2.75, -2.5],"scale": [0.4, 0.4, 0.4]},"thirdperson_lefthand": {"rotation": [0, 0, -55],"translation": [0, 2.75, -2.5],"scale": [0.4, 0.4, 0.4]},"firstperson_righthand": {"rotation": [0, 45, 0],"scale": [0.4, 0.4, 0.4]},"ground": {"translation": [0, 3.25, 0],"scale": [0.4, 0.4, 0.4]},"gui": {"rotation": [28, 45, 0],"scale": [0.6, 0.6, 0.6]}}}')
+                file.write('{"credit": "Made with mDirt","textures": {"0": "' + self.blocks[block]["textures"][0] + '","1": "' + self.blocks[block]["textures"][1] + '","2": "' + self.blocks[block]["textures"][2] + '","3": "' + self.blocks[block]["textures"][3] + '","4": "' + self.blocks[block]["textures"][4] + '","5": "' + self.blocks[block]["textures"][5] + '","particle": "' + self.blocks[block]["textures"][0] + '"},"elements": [{"from": [0, 0, 0],"to": [16, 16, 16],"faces": {"north": {"uv": [0, 0, 16, 16], "texture": "#0"},"east": {"uv": [0, 0, 16, 16], "texture": "#1"},"south": {"uv": [0, 0, 16, 16], "texture": "#2"},"west": {"uv": [0, 0, 16, 16], "texture": "#3"},"up": {"uv": [0, 0, 16, 16], "texture": "#4"},"down": {"uv": [0, 0, 16, 16], "texture": "#5"}}}],"display": {"thirdperson_righthand": {"rotation": [0, 0, -55],"translation": [0, 2.75, -2.5],"scale": [0.4, 0.4, 0.4]},"thirdperson_lefthand": {"rotation": [0, 0, -55],"translation": [0, 2.75, -2.5],"scale": [0.4, 0.4, 0.4]},"firstperson_righthand": {"rotation": [0, 45, 0],"scale": [0.4, 0.4, 0.4]},"ground": {"translation": [0, 3.25, 0],"scale": [0.4, 0.4, 0.4]},"gui": {"rotation": [28, 45, 0],"scale": [0.6, 0.6, 0.6]}}}')
                 file.close()
         
     
